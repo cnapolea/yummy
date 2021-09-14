a90767d53031715099536097a033792fef19816d
cnapolea <cjnapoleao@gmail.com>
2021-09-14 20:29:04 +0100

    trial test

--- routes/home/index.js
+++ routes/home/index.js
@@ -4,7 +4,22 @@ const express = require('express');
 const router = express.Router();
 const routeGuard = require('../../middleware/route-guard');
 
+// Importing restaurant model in order to perform CRUD operations
+// const Restaurant = require('../../models/restaurant');
+
 router.get('/', (req, res, next) => {
+  res.render('home/index', { title: 'Tummy In My Tummy!' });
+});
+
+// Post request for the form in the homepage
+router.post('/', (req, res, next) => {
+  // Getting information from homepage form to query for restaurants base on the var names below defined
+  // const { name, city, cousine, food } = req.body;
+
+  /*
+  Restaurant.find({city}).or();
+  */
+
   res.render('home/index', { title: 'Tummy In My Tummy!' });
 });
 
