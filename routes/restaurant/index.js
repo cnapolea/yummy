76fd2ce86ab5772e230f1f07278de2283f318438
cnapolea <cjnapoleao@gmail.com>
2021-09-18 11:17:20 +0100

    Add: New restaurant routes 1 - Add a route to display all restaurants 2 - Add a route to display information regarding indivual restaurants

--- routes/restaurant/index.js
+++ routes/restaurant/index.js
@@ -9,20 +9,50 @@ const axios = require('axios');
 // Function that formats GEOCODING URL
 const GEOCODING_URL = require('./getLngLat');
 
+// import middlewares
+const routeGuard = require('../../middleware/route-guard');
+
+
+router.get('/', (req, res, next) => {}
+  Restaurant.find({limit: 10})
+    .then((restaurants) => {
+      res.render('restaurants', { restaurants });
+    })
+    .catch((error) => next(error));
+);
+
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+  Restaurant.findById(id)
+    .then((restaurant) => {
+      res.render('restaurant', { restaurant });
+    })
+    .catch((error) => next(error));
+});
+
+router.get('/', (req, res, next) => {}
+  Restaurant.find({limit: 10})
+    .then((restaurants) => {
+      res.render('restaurants', { restaurants });
+    })
+    .catch((error) => next(error));
+);
+
+
 // GET request to take to the create form view
 router.get('/create', (req, res, next) => {
   res.render('restaurant/create-restaurant');
 });
 
 // POST request to create a restaurant document in the DB
-router.post('/create', (req, res, next) => {
-  const { name, address1, postalCode, city, country, cousine, price, image } =
+router.post('/create', routeGuard, (req, res, next) => {
+  const { name, address1, postalCode, city, country, cousine, price } =
     req.body;
+  const { image } = req.body;
 
   axios
     .get(GEOCODING_URL(req.body))
     .then((address) => {
-      // console.log(address.data.results[0]);
       const { lat, lng } = address.data.results[0].geometry.location;
       return Restaurant.create({
         name,
