e68a4e5d5ff7f8c7ffa2e713ae8ea8080ff8029c
cnapolea <cjnapoleao@gmail.com>
2021-09-24 22:23:06 +0100

    Add: Style Restaurants cards

--- public/scripts/nearYouDivs.js
+++ public/scripts/nearYouDivs.js
@@ -1,30 +1,54 @@
 function createRestaurantCard(restaurant) {
-  let divElement = document.querySelector('.cards-container');
+  let cardsContainerElement = document.querySelector('.cards-container');
   let restautantCardElement = document.createElement('div');
-  let imgElement = document.createElement('img');
+  let restaurantImageElement = document.createElement('img');
+  let restaurantInformationElement = document.createElement('div');
   let h3NameElement = document.createElement('h3');
   let smallRatingElement = document.createElement('small');
+  let ratingImageElement = document.createElement('img');
+  let restaurantDetailsElement = document.createElement('div');
   let smallPriceElement = document.createElement('small');
   let smallDistanceElement = document.createElement('small');
+  let distanceSpanElement = document.createElement('span');
 
   const restaurantObj = restaurant[0];
   const restaurantDistance = restaurant[1];
 
-  imgElement.src = restaurantObj.image;
-  imgElement.alt = restaurantObj.name;
+  restaurantImageElement.src = restaurantObj.image;
+  restaurantImageElement.alt = restaurantObj.name;
+  ratingImageElement.src = '/images/star.svg';
+  ratingImageElement.alt = 'rating star';
 
   h3NameElement.innerText = restaurantObj.name;
   smallRatingElement.innerText = restaurantObj.rating;
   smallPriceElement.innerText = restaurantObj.price;
 
-  smallDistanceElement.innerHTML = restaurantDistance;
+  smallDistanceElement.innerHTML = restaurantDistance.toPrecision(2);
+  smallDistanceElement.className = 'distance-to-user';
 
   restautantCardElement.className = 'restaurant-card';
+  restaurantInformationElement.className = 'restaurant-information';
+  restaurantDetailsElement.className = 'restaurant-details';
 
-  divElement.appendChild(restautantCardElement);
-  restautantCardElement.appendChild(h3NameElement);
-  restautantCardElement.appendChild(imgElement);
-  restautantCardElement.appendChild(smallRatingElement);
-  restautantCardElement.appendChild(smallPriceElement);
-  restautantCardElement.appendChild(smallDistanceElement);
+  ratingImageElement.className = 'rating-img';
+
+  smallRatingElement.className = 'rating';
+
+  distanceSpanElement.innerHTML = 'km';
+
+  cardsContainerElement.appendChild(restautantCardElement);
+
+  restautantCardElement.appendChild(restaurantImageElement);
+  restautantCardElement.appendChild(restaurantInformationElement);
+
+  restaurantInformationElement.appendChild(h3NameElement);
+  restaurantInformationElement.appendChild(smallRatingElement);
+  restaurantInformationElement.appendChild(smallDistanceElement);
+  restaurantInformationElement.appendChild(restaurantDetailsElement);
+
+  smallDistanceElement.appendChild(distanceSpanElement);
+
+  smallRatingElement.appendChild(ratingImageElement);
+
+  restaurantDetailsElement.appendChild(smallPriceElement);
 }
