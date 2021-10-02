3e9a547a562e75517bfe895bb802dc72009700e0
cnapolea <cjnapoleao@gmail.com>
2021-10-02 04:03:28 +0100

    Upgrade: Button to let user provide access to location 1 - Add button to restaurants to let users allow access to location 2 - Provide fall safety in case user denies access and style it

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -3,51 +3,63 @@
     'load',
     () => {
       let userLocation;
+      const findNearDivElement = document.getElementById(
+        'find-discoveries-near'
+      );
+      const allowLocationAccessBtn = document.querySelector(
+        '#find-discoveries-near button'
+      );
+      const nearYouSection = document.getElementById('near-you');
 
       let getPosition = () =>
         new Promise((resolve, reject) => {
           navigator.geolocation.getCurrentPosition(resolve, reject);
         });
 
-      getPosition()
-        .then((location) => {
-          userLocation = location;
-          const { longitude, latitude } = location.coords;
-          console.log(location);
+      allowLocationAccessBtn.addEventListener('click', () => {
+        findNearDivElement.className = 'hide';
 
-          return fetch(
-            `/restaurants/location?lon=${longitude}&lat=${latitude}`
-          );
-        })
-        .then((data) => {
-          if (data.status === 200) {
-            return data.json();
-          }
-        })
-        .then((data) => {
-          data.forEach(createRestaurantCard);
-          const nearYouElement = document.querySelector('#near-you');
-          const dotsDivElement = document.createElement('div');
-          dotsDivElement.className = 'dots';
+        getPosition()
+          .then((location) => {
+            userLocation = location;
+            const { longitude, latitude } = location.coords;
 
-          const nearYouGliderElement = document.getElementById('glider-2');
-          nearYouElement.appendChild(dotsDivElement);
-          console.log(nearYouElement);
-
-          new Glider(nearYouGliderElement, {
-            slidesToShow: 1,
-            dots: nearYouElement.lastChild,
-            // exactWidth: true,
-            draggable: true,
-            arrows: {
-              prev: '.glider-prev',
-              next: '.glider-next'
+            return fetch(
+              `/restaurants/location?lon=${longitude}&lat=${latitude}`
+            );
+          })
+          .then((data) => {
+            if (data.status === 200) {
+              return data.json();
             }
+          })
+          .then((data) => {
+            data.forEach(createRestaurantCard);
+            const dotsDivElement = document.createElement('div');
+            dotsDivElement.className = 'dots';
+
+            const nearYouGliderElement = document.getElementById('glider-2');
+            nearYouSection.appendChild(dotsDivElement);
+
+            nearYouSection.className = 'show';
+
+            new Glider(nearYouGliderElement, {
+              slidesToShow: 1,
+              dots: nearYouSection.lastChild,
+              // exactWidth: true,
+              draggable: true,
+              arrows: {
+                prev: '.glider-prev',
+                next: '.glider-next'
+              }
+            });
+          })
+          .catch((error) => {
+            findNearDivElement.className = 'show';
+            findNearDivElement.children[1].className = 'hide';
+            findNearDivElement.children[2].className = 'show';
           });
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+      });
     },
     false
   );
